e72b98b942351ba8cac14bfda8cd9e090ceb3fb8
Zuxing_a1816653 <a1816653@adelaide.edu.au>
2026-02-23 22:43:15 +1030

    initial

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { adminOnly, protect } = require('../middlewares/authMiddleware');
+const { getUsers, getUserById, deleteUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+// User Management Routes
+router.get("/", protect, adminOnly, getUsers); // Get All Users (Admin Only)
+router.get("/:id", protect, getUserById); // get a specific user
+router.delete("/:id", protect, adminOnly, deleteUser); // Delete User (Admin Only)
+
+module.exports = router;
